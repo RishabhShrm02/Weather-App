ae65b6eadde9268e4bcbc3fc03132772aecd7cdf
RishabhShrm02 <143181129+RishabhShrm02@users.noreply.github.com>
2024-08-03 21:25:39 +0530

    Weather Application

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const apikey="0e5eac40651fa9366b6f4331ceb85a44";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+    const response=await fetch(apiUrl + city + `&appid=${apikey}`);
+    var data=await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city ").innerHTML=data.name;
+    document.querySelector(".temp ").innerHTML=Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity ").innerHTML=data.main.humidity + "%";
+    document.querySelector(".wind ").innerHTML=data.wind.speed + "km/h";
+} 
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
